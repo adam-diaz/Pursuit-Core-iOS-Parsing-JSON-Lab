a4ded9e36ba1768a7b7b303c7bfddd0aa6e76498
Adán Diaz <adamdiaz@pursuit.org>
2019-12-05 12:45:00 -0500

    created json files

---
+++ ParsingJSON/ParsingJSON/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  ParsingJSON
+//
+//  Created by Adam Diaz on 12/5/19.
+//  Copyright © 2019 Adam Diaz. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    
+}
